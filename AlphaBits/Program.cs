b082bc083b2c86ba20602e940837e2b90602e4fa
shupac800 <davidshuman@gmail.com>
2016-04-21 11:00:52 -0500

    MVP working, with easter egg

--- AlphaBits/Program.cs
+++ AlphaBits/Program.cs
@@ -6,10 +6,66 @@ using System.Threading.Tasks;
 
 namespace AlphaBits
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            string s = "";
+            string characters = "";
+            bool getInput = true;
+
+            while(getInput)
+            { 
+                ConsoleKeyInfo cki = Console.ReadKey(true);
+                char c = cki.KeyChar;
+
+                if (c == '=')  // is this the secret easter egg character?
+                {
+                    Console.Clear();
+                    Functions.display(characters);
+                    s = String.Format("Boo yah!! Secret Easter egg character!");
+                    Functions.display(s);
+                    continue;
+                }
+
+                // is this a character a-z ?
+                if (c < 'a' || c > 'z')
+                {
+                    s = String.Format("Error: '{0}' isn't a lower-case letter!",c);
+                    Functions.display(s);
+                    continue;
+                }
+
+                // has this letter already been entered?
+                if (characters.IndexOf(c) > -1)
+                {
+                    s = String.Format("Error: this letter has already been entered");
+                    Functions.display(s);
+                    continue;
+                }
+
+                characters = Functions.addChar(characters, c);  // add inputted character to main string
+
+                // do display routine
+                Console.Clear();
+                //Console.WriteLine(characters);
+                int charactersCorrect = Functions.countCharacters(characters);
+                string letterz = charactersCorrect == 1 ? " letter" : " letters";
+                s = String.Format(charactersCorrect + letterz + " correct");
+                Functions.display(s);
+                if (charactersCorrect == 13)
+                {
+                    s = String.Format("Oooh! Halfway there! Keep going!!");
+                    Functions.display(s);
+                }
+                if (charactersCorrect == 26)
+                {
+                    s = String.Format("You are a master of the alphabet.\n");
+                    Functions.display(s);
+                    getInput = false;
+                }
+            }
+
         }
     }
 }
